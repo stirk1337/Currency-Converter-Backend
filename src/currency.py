07736b367f692ad7236ebdd6b2e29892ac285eed
Ramil <stirk2281337@gmail.com>
2022-03-18 00:14:03 +0500

    added async request

--- src/currency.py
+++ src/currency.py
@@ -1,22 +1,28 @@
-import urllib.request
-import json
+import aiohttp
+import asyncio
+
+
+async def request():
+    async with aiohttp.ClientSession() as session:
+        async with session.get('https://www.cbr-xml-daily.ru/daily_json.js') as resp:
+            return await resp.json(content_type=None)
 
 
 def get_currency_based_on_rur():
-    with urllib.request.urlopen("https://www.cbr-xml-daily.ru/daily_json.js") as url:
-        data = json.loads(url.read())
-        curs_list = []
-        curs_value = {}
-        for currencies in data['Valute']:
-            curs_list.append(currencies)
-            cur = data['Valute'][currencies]
-            curs_value[currencies] = cur['Value']/cur['Nominal']
-        pairs = {}
-        for i in range(len(curs_list)):
-            pairs[curs_list[i] + 'RUR'] = curs_value[curs_list[i]]
-            for j in range(len(curs_list)):
-                pairs[curs_list[i] + curs_list[j]] = curs_value[curs_list[i]] / curs_value[curs_list[j]]
-        return pairs
+    loop = asyncio.get_event_loop()
+    data = loop.run_until_complete(request())
+    curs_list = []
+    curs_value = {}
+    for currencies in data['Valute']:
+        curs_list.append(currencies)
+        cur = data['Valute'][currencies]
+        curs_value[currencies] = cur['Value'] / cur['Nominal']
+    pairs = {}
+    for i in range(len(curs_list)):
+        pairs[curs_list[i] + 'RUR'] = curs_value[curs_list[i]]
+        for j in range(len(curs_list)):
+            pairs[curs_list[i] + curs_list[j]] = curs_value[curs_list[i]] / curs_value[curs_list[j]]
+    return pairs
 
 
 if __name__ == "__main__":
